1adc5c49f336d4ab617f7260bd81c2760279658d
Clay2605293 <a01220835@tec.mx>
2024-11-04 08:49:03 -0600

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import Login from './Login';
 import Registro from './Registro';
 import Dashboard from './Dashboard';
 import AltaProducto from './AltaProducto';
-import SendMessage from './SendMessage';
-
+import SendMessage from './SendMessage'; // Supongamos que esta es la nueva página para enviar mensajes
+import Navbar from './Navbar'; // Importa el Navbar
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
