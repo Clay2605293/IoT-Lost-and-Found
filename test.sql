da35dff39e6bedd483ed795dce62543e773d57fe
Clay2605293 <a01220835@tec.mx>
2024-11-07 01:40:28 -0600

    SQL

--- test.sql
+++ test.sql
@@ -26,12 +26,14 @@ DROP TABLE IF EXISTS `Objects`;
 CREATE TABLE `Objects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Matricula` varchar(9) NOT NULL,
-  `Status` varchar(20) NOT NULL,
-  `Foto` varchar(255) DEFAULT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Status` enum('perdido','encontrado') DEFAULT 'encontrado',
+  `Type` enum('Dispositivo electrónico','Mochila','Recipiente de comida','Recipiente de bebida','Objeto personal','Otros') NOT NULL,
+  `CreatedAt` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `Matricula` (`Matricula`),
   CONSTRAINT `Objects_ibfk_1` FOREIGN KEY (`Matricula`) REFERENCES `Users` (`Matricula`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,36 +42,14 @@ CREATE TABLE `Objects` (
 
 LOCK TABLES `Objects` WRITE;
 /*!40000 ALTER TABLE `Objects` DISABLE KEYS */;
+INSERT INTO `Objects` VALUES
+(1,'A01220835','Mochila','encontrado','Mochila','2024-11-07 04:06:00'),
+(2,'A01220835','Laptop Dell','encontrado','Dispositivo electrónico','2024-11-07 04:07:13'),
+(3,'A01220835','Botella Azul','encontrado','Recipiente de bebida','2024-11-07 04:07:13'),
+(4,'A01220835','Mochila Negra','encontrado','Mochila','2024-11-07 04:07:13'),
+(5,'A01220835','Tupper Rojo','encontrado','Recipiente de comida','2024-11-07 04:07:13'),
+(6,'A01220835','Llaves de auto','encontrado','Objeto personal','2024-11-07 04:07:13');
 /*!40000 ALTER TABLE `Objects` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Reports`
---
-
-DROP TABLE IF EXISTS `Reports`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Reports` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `idObject` int(11) NOT NULL,
-  `GPS` varchar(50) DEFAULT NULL,
-  `Status` varchar(20) DEFAULT NULL,
-  `Building` varchar(50) DEFAULT NULL,
-  `Classroom` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idObject` (`idObject`),
-  CONSTRAINT `Reports_ibfk_1` FOREIGN KEY (`idObject`) REFERENCES `Objects` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Reports`
---
-
-LOCK TABLES `Reports` WRITE;
-/*!40000 ALTER TABLE `Reports` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Reports` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -97,8 +77,7 @@ CREATE TABLE `Users` (
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
 INSERT INTO `Users` VALUES
-('A01220835','$2b$10$D0yUKpOGDmUDrsZWHI1nneIdcyQzA8Rx9cEDfT8lTgv295DA7Mymy','Clay','','',0),
-('A01220836','$2b$10$bO0R6Izx5ZhJS3pag35fG.QzHUJ8pz.eCnMlAexfpRZX39WfXqWM2','Prueba','','',0);
+('A01220835','$2b$10$.a7ijsKIr1LrDjfH0T8Mu.s.X40zYn9WCUYEKUK/Jq5VuOOKtHpCG','Clay','5648954564','dfes@ds.com',0);
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-24 21:03:44
+-- Dump completed on 2024-11-07  1:40:16
